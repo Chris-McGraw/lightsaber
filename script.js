13c38d925d9748af5835f4a5245c85fbff5fe70f
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-01-17 12:37:51 -0500

    random-blade-color-update-2

--- script.js
+++ script.js
@@ -68,13 +68,18 @@ $(document).ready(function(){
     saberIdle.currentTime = 0;
   }
 
-  var randomColor = ["#F0FEF0", "blue", "red"];
+  var randomBladeColor = ["#F0FEF0", "#F6FBF6", "#FEFAF6"];
+  var randomGlowColor = ["0px -6px 6px 8px rgba(182,252,182, 0.9)",
+  "0px -6px 6px 8px rgba(90,216,253, 0.9)",
+  "0px -6px 6px 8px rgba(231,120,93, 0.9)"];
 
   function getColor() {
     var randomNum = Math.floor((Math.random() * 3));
-    $("#blade").css("background-color", randomColor[randomNum]);
-    $(".blade-tip").css("background-color", randomColor[randomNum]);
-    $("#blade-flash").css("background-color", randomColor[randomNum]);
+    $("#blade").css("background-color", randomBladeColor[randomNum]);
+    $(".blade-tip").css("background-color", randomBladeColor[randomNum]);
+    $("#blade-flash").css("background-color", randomBladeColor[randomNum]);
+
+    $(".extend").css("box-shadow", randomGlowColor[randomNum]);
   }
 
 
@@ -86,26 +91,27 @@ $(document).ready(function(){
       $(".blade-tip").toggleClass("hidden");
 
       getColor();
-      
+
       /* $(".extend").one("transitionend", function(){
         $(".extend").css("background-color", "blue")
-        $(".extend").css("box-shadow", "0px -6px 6px 8px rgba(90,216,253, 0.9)")
+        $(".extend").css("box-shadow", "0px -6px 6px 8px rgba rgba(90,216,253, 0.9)")
       }); */
 
       saberStatus = true;
     }
+
     else if (saberStatus === true) {
       playOff();
       stopIdle();
       $("#blade").toggleClass("extend");
       $(".blade-tip").toggleClass("hidden");
 
-      /* $("#blade").one("transitionend", function(){
+   /* $("#blade").one("transitionend", function(){
         $("#blade").css("background-color", "#F6FBFC");
         $(".blade-tip").css("background-color", "#F6FBFC");
 
-        $(".extend").css("background-color", "blue")
-        $(".extend").css("box-shadow", "0px -6px 6px 8px rgba(90,216,253, 0.9)")
+        $(".extend").css("background-color", "blue");
+        $(".extend").css("box-shadow", "0px -6px 6px 8px rgba(90,216,253, 0.9)");
       }); */
 
       saberStatus = false;
